5a98e41e670bed0159f2b6a7403ed5d0f658942f
Janet Mugambi <janet.mugambi@equitybank.co.ke>
2025-09-10 13:04:02 +0300

    Create component and implement @input decorator

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {COURSES} from '../db-data';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    standalone: false
+})
+export class AppComponent {
+
+    coreCourse =COURSES[0];
+    rjxCourse=COURSES[1];
+    ngrxCourse=COURSES[2];  
+
+
+}
